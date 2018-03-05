f2ab7b5733f1f2fe845b016e9216086504ecdff4
lordey <lord.dey68@gmail.com>
2018-03-05 22:13:52 +0700

    first commit

---
+++ OpenVPN.sh
@@ -0,0 +1,134 @@
+# install webserver
+apt-get -y install nginx php5-fpm php5-cli
+
+# install essential package
+echo "mrtg mrtg/conf_mods boolean true" | debconf-set-selections
+apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs openvpn vnstat less screen psmisc apt-file whois ptunnel ngrep mtr git zsh mrtg snmp snmpd snmp-mibs-downloader unzip unrar rsyslog debsums rkhunter
+apt-get -y install build-essential
+
+# update apt-file
+apt-file update
+
+# install webserver
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/lordey/webserver/master/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>Dey Agustian</pre>" > /home/vps/public_html/index.html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/lordey/webserver/master/vps.conf"
+service nginx restart
+
+#install OpenVPN
+apt-get -y install openvpn iptables openssl
+cp -R /usr/share/doc/openvpn/examples/easy-rsa/ /etc/openvpn
+# easy-rsa
+if [[ ! -d /etc/openvpn/easy-rsa/2.0/ ]]; then
+	wget --no-check-certificate -O ~/easy-rsa.tar.gz https://github.com/OpenVPN/easy-rsa/archive/2.2.2.tar.gz
+    tar xzf ~/easy-rsa.tar.gz -C ~/
+    mkdir -p /etc/openvpn/easy-rsa/2.0/
+    cp ~/easy-rsa-2.2.2/easy-rsa/2.0/* /etc/openvpn/easy-rsa/2.0/
+    rm -rf ~/easy-rsa-2.2.2
+    rm -rf ~/easy-rsa.tar.gz
+fi
+cd /etc/openvpn/easy-rsa/2.0/
+# benarkan errornya
+cp -u -p openssl-1.0.0.cnf openssl.cnf
+# ganti bits
+sed -i 's|export KEY_SIZE=1024|export KEY_SIZE=2048|' /etc/openvpn/easy-rsa/2.0/vars
+sed -i 's|export KEY_COUNTRY="US"|export KEY_COUNTRY="ID"|' /etc/openvpn/easy-rsa/2.0/vars
+sed -i 's|export KEY_PROVINCE="CA"|export KEY_PROVINCE="West Java"|' /etc/openvpn/easy-rsa/2.0/vars
+sed -i 's|export KEY_CITY="SanFrancisco"|export KEY_CITY="Majalengka"|' /etc/openvpn/easy-rsa/2.0/vars
+sed -i 's|export KEY_ORG="Fort-Funston"|export KEY_ORG="warungsiganteng.com"|' /etc/openvpn/easy-rsa/2.0/vars
+sed -i 's|export KEY_EMAIL="me@myhost.mydomain"|export KEY_EMAIL="admin@warungsiganteng.com"|' /etc/openvpn/easy-rsa/2.0/vars
+sed -i 's|export KEY_EMAIL=mail@host.domain|export KEY_EMAIL=admin@warungsiganteng.com|' /etc/openvpn/easy-rsa/2.0/vars
+sed -i 's|export KEY_CN=changeme|export KEY_CN="warungsiganteng.com"|' /etc/openvpn/easy-rsa/2.0/vars
+sed -i 's|export KEY_NAME=changeme|export KEY_NAME=warungsiganteng.com|' /etc/openvpn/easy-rsa/2.0/vars
+sed -i 's|export KEY_OU=changeme|export KEY_OU=warungsiganteng.com|' /etc/openvpn/easy-rsa/2.0/vars
+# Buat PKI
+. /etc/openvpn/easy-rsa/2.0/vars
+. /etc/openvpn/easy-rsa/2.0/clean-all
+# Buat Sertifikat
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" --initca $*
+# buat key server
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" --server server
+# seting KEY CN
+export EASY_RSA="${EASY_RSA:-.}"
+"$EASY_RSA/pkitool" client
+# DH params
+. /etc/openvpn/easy-rsa/2.0/build-dh
+# Setting Server
+cat > /etc/openvpn/server.conf <<-END
+port 1194
+proto tcp
+dev tun
+tun-mtu 1500
+tun-mtu-extra 32
+mssfix 1450
+ca /etc/openvpn/ca.crt
+cert /etc/openvpn/server.crt
+key /etc/openvpn/server.key
+dh /etc/openvpn/dh2048.pem
+plugin /usr/lib/openvpn/openvpn-auth-pam.so /etc/pam.d/login
+client-cert-not-required
+username-as-common-name
+server 192.168.100.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+push "redirect-gateway def1"
+push "dhcp-option DNS 8.8.8.8"
+push "dhcp-option DNS 8.8.4.4"
+push "route-method exe"
+push "route-delay 2"
+keepalive 5 30
+cipher AES-128-CBC
+comp-lzo
+persist-key
+persist-tun
+status server-vpn.log
+verb 3
+END
+cd /etc/openvpn/easy-rsa/2.0/keys
+cp ca.crt ca.key dh2048.pem server.crt server.key /etc/openvpn
+cd /etc/openvpn/
+
+
+#Create OpenVPN Config
+mkdir -p /home/vps/public_html
+cat > /home/vps/public_html/client.ovpn <<-END
+client
+proto tcp
+persist-key
+persist-tun
+dev tun
+pull
+comp-lzo
+ns-cert-type server
+verb 3
+mute 2
+mute-replay-warnings
+auth-user-pass
+redirect-gateway def1
+script-security 2
+route 0.0.0.0 0.0.0.0
+route-method exe
+route-delay 2
+remote $MYIP 1194
+cipher AES-128-CBC
+END
+echo '<ca>' >> /home/vps/public_html/client.ovpn
+cat /etc/openvpn/ca.crt >> /home/vps/public_html/client.ovpn
+echo '</ca>' >> /home/vps/public_html/client.ovpn
+cd /home/vps/public_html/
+tar -czf /home/vps/public_html/openvpn.tar.gz client.ovpn
+tar -czf /home/vps/public_html/client.tar.gz client.ovpn
+cd
+
+# set ipv4 forward
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
+sed -i 's|net.ipv4.ip_forward=0|net.ipv4.ip_forward=1|' /etc/sysctl.conf
+
+# Restart openvpn
+/etc/init.d/openvpn restart
